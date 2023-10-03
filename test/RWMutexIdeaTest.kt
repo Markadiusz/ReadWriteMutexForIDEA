57ed041782149e19a0babbd35216ffe9b53affad
Markadiusz <arkadiusz.czarkowski2212@gmail.com>
2023-10-03 14:57:35 +0200

    Added basic tests

--- test/RWMutexIdeaTest.kt
+++ test/RWMutexIdeaTest.kt
@@ -1,3 +1,43 @@
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.yield
+import kotlin.test.Test
+
 class RWMutexIdeaTest : TestBase() {
-    // TODO: write tests
+
+    @Test
+    fun cancelOnAcquiredWritePermitTrue() = runTest {
+        val m = RWMutexIdea()
+
+        val readJob = launch {
+            m.acquireReadPermit(true)
+            expect(1)
+        }
+        yield()
+
+        val writeJob = launch {
+            m.acquireWritePermit()
+            expect(2)
+        }
+        writeJob.join()
+
+        finish(3)
+    }
+
+    @Test
+    fun cancelOnAcquiredWritePermitFalse() = runTest {
+        val m = RWMutexIdea()
+        m.acquireReadPermit(false)
+
+        expect(1)
+
+        val writeJob = launch {
+            m.acquireWritePermit()
+            expectUnreached()
+        }
+        yield()
+
+        writeJob.cancel()
+
+        finish(2)
+    }
 }
