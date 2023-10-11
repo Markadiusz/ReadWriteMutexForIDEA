afb5ddc4bf5d91c737cf37c7cee53fcaeeb2e5e3
Markadiusz <arkadiusz.czarkowski2212@gmail.com>
2023-10-11 09:14:19 +0200

    Fixed cancellation tests

--- test/RWMutexIdeaTest.kt
+++ test/RWMutexIdeaTest.kt
@@ -1,3 +1,4 @@
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.yield
 import kotlin.test.Test
@@ -8,36 +9,49 @@ class RWMutexIdeaTest : TestBase() {
     fun cancelOnAcquiredWritePermitTrue() = runTest {
         val m = RWMutexIdea()
 
-        val readJob = launch {
-            m.acquireReadPermit(true)
-            expect(1)
+        launch {
+            var readPermit: Permit? = null
+            try {
+                readPermit = m.acquireReadPermit(true)
+                expect(1)
+                delay(1_000_000L)
+            }
+            finally {
+                expect(3)
+                readPermit!!.release()
+            }
         }
         yield()
 
         val writeJob = launch {
             expect(2)
             m.acquireWritePermit()
+            expect(4)
         }
         writeJob.join()
 
-        finish(3)
+        finish(5)
     }
 
     @Test
     fun cancelOnAcquiredWritePermitFalse() = runTest {
         val m = RWMutexIdea()
+
         m.acquireReadPermit(false)
 
         expect(1)
 
         val writeJob = launch {
+            expect(2)
             m.acquireWritePermit()
             expectUnreached()
         }
         yield()
 
+        expect(3)
+
         writeJob.cancel()
 
-        finish(2)
+        finish(4)
     }
 }
